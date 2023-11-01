5dbf19bbfde487c98091416e843109e7fd8449ed
Иванов В <oomnpe@gmail.com>
2023-11-01 23:34:17 +0300

    Ex4: GET-запрос

---  src/test/java/HelloWorldTest.java
+++  src/test/java/HelloWorldTest.java
@@ -1,11 +1,16 @@
 package test.java;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
 
     @Test
     public void testHelloWorld(){
-        System.out.println("Hello from Vasya");
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.prettyPrint();
     }
 }
