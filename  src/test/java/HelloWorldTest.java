50279b8013e18913662fd9404255cbe9d9156deb
Иванов В <oomnpe@gmail.com>
2023-11-01 22:26:14 +0300

    HWTest

---
+++  src/test/java/HelloWorldTest.java
@@ -0,0 +1,11 @@
+package test.java;
+
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldTest {
+
+    @Test
+    public void testHelloWorld(){
+        System.out.println("Hello world");
+    }
+}
