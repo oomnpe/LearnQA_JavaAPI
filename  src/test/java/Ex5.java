250c8167f15527f81b32025c8e417317dd35a492
Иванов В <oomnpe@gmail.com>
2023-11-08 23:20:55 +0300

    Ex5: Парсинг JSON

---
+++  src/test/java/Ex5.java
@@ -0,0 +1,21 @@
+package test.java;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
+public class Ex5 {
+    @Test
+    public void ex5(){
+        JsonPath response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .jsonPath();
+
+        ArrayList<LinkedHashMap<String, Object>> jsonPaths = response.get("messages");
+        LinkedHashMap<String, Object> secondAnswer = jsonPaths.get(1);
+        System.out.println(secondAnswer.get("message"));
+    }
+}
