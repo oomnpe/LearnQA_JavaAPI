61bf98fa6058d446721245799eb1400dfa53f33e
Иванов В <oomnpe@gmail.com>
2023-11-09 09:13:12 +0300

    Ex6: Редирект

---
+++  src/test/java/Ex6.java
@@ -0,0 +1,28 @@
+package test.java;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Header;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+public class Ex6 {
+    @Test
+    public void ex6() {
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+
+        String location = String.valueOf(response.getHeaders().get("Location").getValue());
+
+        System.out.println(location);
+    }
+}
