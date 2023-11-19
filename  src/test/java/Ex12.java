29d496a40f76307ccd0367258143ee54e757f8b0
Иванов В <oomnpe@gmail.com>
2023-11-19 20:00:18 +0300

    Ex12: Тест запроса на метод header

---
+++  src/test/java/Ex12.java
@@ -0,0 +1,21 @@
+package test.java;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Ex12 {
+
+    @Test
+    public void e12() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/homework_header")
+                .andReturn();
+
+        String header = response.getHeaders().get("x-secret-homework-header").getValue();
+
+        assertEquals("Some secret value", header, "Bad answer");
+    }
+}
