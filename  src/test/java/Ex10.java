30b51a6c02f646791378c29d3ac15b31284f28ed
Иванов В <oomnpe@gmail.com>
2023-11-19 19:41:29 +0300

    удалил лишние импорты

---  src/test/java/Ex10.java
+++  src/test/java/Ex10.java
@@ -1,9 +1,5 @@
 package test.java;
 
-import com.sun.tools.javac.util.Assert;
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
