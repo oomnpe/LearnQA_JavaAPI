2648d576eec0c44b9a5e0869e7c60106287e71ed
Иванов В <oomnpe@gmail.com>
2023-11-19 19:51:22 +0300

    Ex11: Тест запроса на метод cookie

---
+++  src/test/java/Ex11.java
@@ -0,0 +1,24 @@
+package test.java;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Cookie;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Ex11 {
+
+    @Test
+    public void e11() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/homework_cookie")
+                .andReturn();
+
+        Map<String, String> respCookies = response.getCookies();
+
+        assertEquals("hw_value", respCookies.get("HomeWork"), "Bad answer");
+    }
+}
