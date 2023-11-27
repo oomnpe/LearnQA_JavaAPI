4b2be99938605a0224e5537a4273e5187154dfb8
Иванов В <oomnpe@gmail.com>
2023-11-27 22:19:21 +0300

    Ex15: Тесты на метод user

---  src/test/java/lib/DataGenerator.java
+++  src/test/java/lib/DataGenerator.java
@@ -3,6 +3,7 @@ package test.java.lib;
 import java.text.SimpleDateFormat;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class DataGenerator {
     public static String getRandomEmail() {
@@ -36,4 +37,25 @@ public class DataGenerator {
         return userData;
     }
 
+    public static Map<String, String> getRegistrationDataWithoutOneField(String missingField) {
+        Map<String, String> defaultValues = DataGenerator.getRegistrationData();
+
+        Map<String, String> userData = new HashMap<>();
+        String[] keys = {"email", "password", "username", "firstName", "lastName"};
+        for (String key : keys) {
+            if (!missingField.equals(key)) {
+                userData.put(key, defaultValues.get(key));
+            }
+        }
+        return userData;
+    }
+
+    public static String generateString(String characters, int length) {
+        Random rng = new Random();
+        char[] text = new char[length];
+        for (int i = 0; i < length; i++) {
+            text[i] = characters.charAt(rng.nextInt(characters.length()));
+        }
+        return new String(text);
+    }
 }
---  src/test/java/tests/UserRegisterTest.java
+++  src/test/java/tests/UserRegisterTest.java
@@ -1,8 +1,12 @@
 package test.java.tests;
 
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import test.java.lib.ApiCoreRequests;
 import test.java.lib.Assertions;
 import test.java.lib.BaseTestCase;
 import test.java.lib.DataGenerator;
@@ -11,6 +15,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class UserRegisterTest extends BaseTestCase {
+    private final ApiCoreRequests apiCoreRequests = new ApiCoreRequests();
 
     @Test
     public void testCreateUserWithExistingEmail() {
@@ -46,5 +51,66 @@ public class UserRegisterTest extends BaseTestCase {
 
         Assertions.assertResponseCodeEquals(responseCreateAuth, 200);
         Assertions.assertJsonHasField(responseCreateAuth, "id");
+    }
+
+    /**
+     * Создание пользователя с некорректным email - без символа @
+     */
+    @Test
+    public void testCreateUserWithIncorrectEmail() {
+        String email = "vinkotovexample.com";
+
+        Map<String, String> userData = new HashMap<>();
+        userData.put("email", email);
+        userData = DataGenerator.getRegistrationData(userData);
+
+        Response responseCreateAuth = apiCoreRequests.makePostRequest("https://playground.learnqa.ru/api/user/", userData);
+
+        Assertions.assertResponseTextEquals(responseCreateAuth, "Invalid email format");
+    }
+
+    /**
+     * Создание пользователя без указания одного из полей
+     */
+    @ParameterizedTest
+    @ValueSource(strings = {"email", "password", "username", "firstName", "lastName"})
+    public void testCreateUserWithoutOneField(String missingField) {
+        Map<String, String> userData = DataGenerator.getRegistrationDataWithoutOneField(missingField);
+
+        Response responseCreateAuth = apiCoreRequests.makePostRequest("https://playground.learnqa.ru/api/user/", userData);
+
+        Assertions.assertResponseTextEquals(responseCreateAuth, "The following required params are missed: " + missingField);
+    }
+
+    /**
+     * Создание пользователя с очень коротким именем в один символ
+     */
+    @Test
+    public void testCreateUserWithShortName() {
+        String userName = "v";
+
+        Map<String, String> userData = new HashMap<>();
+        userData.put("username", userName);
+        userData = DataGenerator.getRegistrationData(userData);
+
+        Response responseCreateAuth = apiCoreRequests.makePostRequest("https://playground.learnqa.ru/api/user/", userData);
+
+        Assertions.assertResponseTextEquals(responseCreateAuth, "The value of 'username' field is too short");
+    }
+
+    /**
+     * Создание пользователя с очень длинным именем - длиннее 250 символов
+     */
+    @Test
+    public void testCreateUserWithLongName() {
+        String userName = DataGenerator.generateString("qwelkjoiu", 251);
+
+        Map<String, String> userData = new HashMap<>();
+        userData.put("username", userName);
+        userData = DataGenerator.getRegistrationData(userData);
+
+        Response responseCreateAuth = apiCoreRequests.makePostRequest("https://playground.learnqa.ru/api/user/", userData);
+
+        Assertions.assertResponseTextEquals(responseCreateAuth, "The value of 'username' field is too long");
     }
 }
